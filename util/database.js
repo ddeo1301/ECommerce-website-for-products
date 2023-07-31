cb644650af2575e262149caa721a9e4f435ab061
ddeo1301 <divyanshudeo1301@gmail.com>
2023-07-31 09:38:03 +0530

    getting started with mongodb and creating products

--- util/database.js
+++ util/database.js
@@ -1,96 +1,34 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-/**
- * mongo
- *
- * event handling for mongo db
- */
+let _db;
 
-const mongoose = require('mongoose'),
-  dbURI = constants.mongo.uri;
+const mongoConnect = callback => {
+  MongoClient.connect(
+    //'mongodb+srv://divyanshudeo1301:Divyanshu97@ecartwebsite.t1egsr1.mongodb.net/shop?retryWrites=true&w=majority'
+    "mongodb+srv://divyanshudeo1301:Divyanshu97@ecartwebsite.t1egsr1.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log("error in mongodb connection");
+      console.log(err);
+      throw err;
+    });
+};
 
-mongoose.set('bufferCommands', false);
+const getDb = () => {
+  if(_db){
+    return _db;
+  }
+  throw 'No database connected'
+}
 
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  autoReconnect: true,
-  reconnectTries: constants.mongo.connRetries,
-  reconnectInterval: constants.mongo.connRetriesInterval,
-  auth: {
-    user: constants.mongo.user,
-    password: constants.mongo.password
-  }
-});
-
-mongoose.connection.on('connected', function () {
-  LogService.access('mongo', 'onConnected', null, 'Connected to mongodb');
-});
-
-mongoose.connection.on('error', function (err) {
-  ErrorService.connectionError(err);
-});
-
-mongoose.connection.on('disconnected', function () {
-  ErrorService.connectionError();
-});
-
-process.on('SIGINT', function () {
-  mongoose.connection.close(function () {
-    process.exit(0);
-  });
-});
-
-require('./../models');
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const mongodb = require('mongodb');
-// const MongoClient = mongodb.MongoClient;
-
-// let _db;
-
-// const mongoConnect = callback => {
-//   MongoClient.connect(
-//     'mongodb+srv://divyanshudeo1301:Divyanshu97@ecommerce.kp9m4pp.mongodb.net/shop?retryWrites=true&w=majority'
-//   )
-//     .then(client => {
-//       console.log('Connected!');
-//       _db = client.db();
-//       callback();
-//     })
-//     .catch(err => {
-//       console.log("error in mongodb connection");
-//       console.log(err);
-//       throw err;
-//     });
-// };
-
-// const getDb = () => {
-//   if(_db){
-//     return _db;
-//   }
-//   throw 'No database connected'
-// }
-
-// exports.mongoConnect = mongoConnect;
-// exports.getDb = getDb;
